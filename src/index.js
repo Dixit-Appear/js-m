78f8455fc0fbe16ee8558801ec1a4740635b3177
Edoardo Olivieri <diddo.42@hotmail.it>
2019-05-07 17:06:48 +0100

    adding plugings

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 // console.log("Hello from src/index.js!");
-const results = document.querySelector("#results");
-
-const searchMovies = (keyword) => {
-  const apiURL = `http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
-  fetch(apiURL)
-    .then(response => response.json())
-    .then((data) => {
-      data.Search.forEach((result) => {
-        const movie = `<li>
-          <img src="${result.Poster}" alt="">
-          <p>${result.Title}</p>
-        </li>`;
-        results.insertAdjacentHTML("beforeend", movie);
-      });
-    });
-};
+import { fetchMovies, addMovieList } from './movie';
 
 const searchForm = document.querySelector('#search-movies');
 
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  results.innerHTML = '';
-  const keyword = document.querySelector("#keyword").value;
-  searchMovies(keyword);
-});
+searchForm.addEventListener("submit", addMovieList);
 
-searchMovies("blade runner");
+fetchMovies("blade runner");
---
+++ src/movie.js
@@ -0,0 +1,27 @@
+const results = document.querySelector("#results");
+
+const updateResults = (data) => {
+  data.Search.forEach((movie) => {
+    const newMovie = `<li>
+      <img src="${movie.Poster}" alt="">
+      <p>${movie.Title}</p>
+    </li>`;
+    results.insertAdjacentHTML("beforeend", newMovie);
+  });
+};
+
+const fetchMovies = (keyword) => {
+  const apiURL = `http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
+  fetch(apiURL)
+    .then(response => response.json())
+    .then(updateResults);
+};
+
+const addMovieList = (event) => {
+  event.preventDefault();
+  results.innerHTML = '';
+  const keyword = document.querySelector("#keyword").value;
+  fetchMovies(keyword);
+};
+
+export { fetchMovies, addMovieList };
