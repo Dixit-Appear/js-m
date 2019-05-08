b882cadef017d30de362087e7da5101dce4c09db
Edoardo Olivieri <diddo.42@hotmail.it>
2019-05-08 11:50:16 +0100

    adding plugin npm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // console.log("Hello from src/index.js!");
 import { fetchMovies, addMovieList } from './movie';
+import { initSortable } from './plugins/init_sortable';
 
 const searchForm = document.querySelector('#search-movies');
 
 searchForm.addEventListener("submit", addMovieList);
 
 fetchMovies("blade runner");
+initSortable();
